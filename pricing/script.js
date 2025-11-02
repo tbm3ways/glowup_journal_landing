5473721ec02cf0961d5270974ad7adda77c4aa99
tbm3ways <tbm.3ways@gmail.com>
2025-11-02 18:24:46 +0500

    pricing_page_1

---
+++ pricing/script.js
@@ -0,0 +1,10 @@
+// Placeholder Paddle integration simulation
+function buyProMonthly() {
+  alert("Redirecting to Paddle Checkout: Pro Monthly Plan");
+  // Example: Paddle.Checkout.open({ product: 12345 });
+}
+
+function buyProYearly() {
+  alert("Redirecting to Paddle Checkout: Pro Yearly Plan");
+  // Example: Paddle.Checkout.open({ product: 67890 });
+}
